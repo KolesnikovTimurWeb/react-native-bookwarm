361df6bc45ad8aa8e69bb517328e770f3e128739
Kolesnikov <kolesnikovtimyrweb@gmail.com>
2025-05-11 12:12:53 +0200

    Cron added

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js";
 import bookRoutes from "./routes/bookRoutes.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+
+job.start();
 
 app.listen(PORT, () => {
   console.log("Server running ", PORT);
